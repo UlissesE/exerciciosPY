e49b44573507e457a75e01cd0f97e8d50474532e
Eduardo <eduardoulisses2006@gmail.com>
2025-09-01 22:12:54 -0300

    mais matrizes

--- 2-semestre/10-matriz_diagonal.py
+++ 2-semestre/10-matriz_diagonal.py
@@ -10,26 +10,75 @@
 
 # print(matriz_diagonal(5))
 
-def criar_matriz(linhas, colunas):
+def criar_matriz_quadrada(tamanho):
     matriz = []
-    for i in range(linhas):
+    
+    for i in range(tamanho):
         linha = []
-        for j in range(colunas):
+        for j in range(tamanho):
             linha.append('.')
         matriz.append(linha)
+    
     return matriz
+
+# print(criar_matriz(5))
 
 def diagonal(tamanho):
-    matriz = criar_matriz(tamanho, tamanho)
+    matriz = criar_matriz_quadrada(tamanho)
+
     for i in range(tamanho):
         matriz[i][i] = "x"
+    
     return matriz
+
+# print(diagonal(5))
 
 def diagonal_inversa(tamanho):
-    matriz = criar_matriz(tamanho, tamanho)
+    matriz = criar_matriz_quadrada(tamanho)
+
     for i in range(tamanho):
-        i = tamanho - 1
-        matriz[i][i] = "x"
+        j = (tamanho - 1) - i
+        matriz[i][j] = 'x'
+
     return matriz
 
-print(diagonal_inversa(5))
+# print(diagonal_inversa(5))
+
+def duas_diagonais(tamanho):
+    matriz = criar_matriz_quadrada(tamanho)
+
+    for i in range(tamanho):
+        j = (tamanho - 1) - i
+        matriz[i][i] = 'x'
+        matriz[i][j] = 'x'
+
+    return matriz
+
+# print(duas_diagonais(5))
+
+
+def matriz_bordas(tamanho):
+    matriz = criar_matriz_quadrada(tamanho)
+
+    for coluna in range(tamanho):
+        matriz[coluna][0] = 'x'
+        matriz[coluna][tamanho - 1] = 'x'
+
+        matriz[0][coluna] = 'x'
+        matriz[tamanho-1][coluna] = 'x'
+        
+    return matriz
+
+# print(matriz_bordas(5))
+
+def coluna(matriz, n_coluna):
+    elementos_coluna = []
+
+    for linha in matriz:
+        elementos_coluna.append(linha[n_coluna])
+
+    return elementos_coluna
+
+print(coluna(duas_diagonais(5),0))
+print(coluna(duas_diagonais(5),1))
+print(coluna(duas_diagonais(5),2))
